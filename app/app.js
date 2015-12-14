de7216efc685377f46d2dc563f3ac8658d892fe8
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-12-14 13:35:29 +0100

    upload new version, gif updated

--- app/app.js
+++ app/app.js
@@ -7,10 +7,22 @@ app.controller('AnimationCtrl', [
     '$window',
     'ngAudio',
     function ($scope, $interval, $timeout, $window, ngAudio) {
-        $scope.sound = ngAudio.load("sounds/DEAF_KEV_-_Invincible.mp3");
+        $scope.sound = ngAudio.load("assets/DEAF_KEV_-_Invincible.mp3");
+        $scope.logo = false;
+        $scope.description = false;
+        $scope.gif = false;
 
         $scope.play = function() {
             $scope.sound.play();
+            $scope.logo = true;
+            $timeout(function () {
+                $scope.logo = false;
+                $scope.description = true;
+            }, 4000);
+            $timeout(function () {
+                $scope.description = false;
+                $scope.gif = true;
+            }, 10500);
         };
 
         $scope.stop = function() {
