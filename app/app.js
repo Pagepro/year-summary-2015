bdf07148a31520a5136ef881f99228b52cdefe1b
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-12-14 14:51:43 +0100

    add font, move styles to separate file

--- app/app.js
+++ app/app.js
@@ -7,10 +7,12 @@ app.controller('AnimationCtrl', [
     '$window',
     'ngAudio',
     function ($scope, $interval, $timeout, $window, ngAudio) {
+        var switcherCouner = 0;
         $scope.sound = ngAudio.load("assets/DEAF_KEV_-_Invincible.mp3");
         $scope.logo = false;
         $scope.description = false;
         $scope.gif = false;
+        $scope.switcher = 0;
 
         $scope.play = function() {
             $scope.sound.play();
@@ -23,6 +25,14 @@ app.controller('AnimationCtrl', [
                 $scope.description = false;
                 $scope.gif = true;
             }, 10500);
+            $interval(function () {
+                switcherCouner++;
+                if (switcherCouner % 2 == 0) {
+                    $scope.switcher = 1;
+                } else {
+                    $scope.switcher = 2.5;
+                }
+            }, 600);
         };
 
         $scope.stop = function() {
