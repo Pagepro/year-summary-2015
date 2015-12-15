26751e5514f45b73933eb212fc59459230794618
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-12-15 14:00:46 +0100

    finitialize project

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+'use strict';
 var app = angular.module('yearSummaryApp', ['ngAudio']);
 
 app.controller('AnimationCtrl', [
@@ -8,11 +9,11 @@ app.controller('AnimationCtrl', [
     'ngAudio',
     function ($scope, $interval, $timeout, $window, ngAudio) {
         var switcherCouner = 0;
-        $scope.sound = ngAudio.load("assets/DEAF_KEV_-_Invincible.mp3");
+        $scope.sound = ngAudio.load("assets/deaf_kev_-_invincible.mp3");
         $scope.logo = false;
         $scope.description = false;
         $scope.gif = false;
-        $scope.switcher = 0;
+        $scope.switcher = false;
         $scope.firstPlay = false;
         $scope.imagesLoaded = 0;
         $scope.imagesLoadingInProgress = true;
@@ -28,7 +29,15 @@ app.controller('AnimationCtrl', [
                 $scope.description = false;
                 $scope.gif = true;
                 $scope.firstPlay = true;
-            }, 10800);
+            }, 10700);
+            $interval(function () {
+                switcherCouner++;
+                if (switcherCouner % 2 === 0) {
+                    $scope.switcher = false;
+                } else {
+                    $scope.switcher = true;
+                }
+            }, 600);
         };
 
         $scope.imageLoaded = function () {
